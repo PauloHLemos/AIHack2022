c0eef92beea514f9399d6814470b78c02efec70d
Paulo Lemos <pl2420@ic.ac.uk>
2022-03-12 15:08:47 +0000

    create loader file

---
+++ loader.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+a = np.load("t1/trajectory_frame0000.npy", allow_pickle=True)
+print(a)
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import numpy as np
+import pickle
+from tenserflow import keras
+
+
+def loader():
+    a = np.load("trajectories/t1/trajectory1_frame0000.npy", allow_pickle=True)
+    print(a)
+
+
 if __name__ == '__main__':
-    print('hello')
+    loader()
 
