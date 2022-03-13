bf55a8add67b19b5e03812a9a4aff29b26c6e72f
Paulo Lemos <pl2420@ic.ac.uk>
2022-03-13 02:16:29 +0000

    add model

---
+++ Model.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+class Model():
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def sequential(self):
+        # define the keras model
+        model = Sequential()
+        model.add(Dense(12, input_dim=8, activation='relu'))
+        model.add(Dense(8, activation='relu'))
+        model.add(Dense(1, activation='sigmoid'))
+        # compile the keras model
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        # fit the keras model on the dataset
+        model.fit(self.X, self.y, epochs=150, batch_size=10)
+        # evaluate the keras model
+        _, accuracy = model.evaluate(self.X, self.y)
+        print('Accuracy: %.2f' % (accuracy*100))
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
+
 from Frame import Frame
+from Model import Model
 
 
-def load(path):
+def load_frames(path):
     frames = []
     for fname in glob(path + "/*"):
         frames.append(np.load(fname, allow_pickle=True))
@@ -28,10 +32,11 @@ def plot(frames):
         fig.canvas.draw()
 
 
-if __name__ == '__main__':
-    frames = load("trajectories/t4")
+def load_data(dir):
+    frames = load_frames(dir)
     # plot(frames)
-    a1 = a2 = []
+    areas1 = []
+    areas2 = []
     relCoords = []
     for f in frames:
         frame = Frame(f)
@@ -40,8 +45,8 @@ if __name__ == '__main__':
             print("more than 2 blobs detected")
         elif len(areas) == 2:
             # update area info
-            a1.append(areas[0])
-            a2.append(areas[1])
+            areas1.append(areas[0])
+            areas2.append(areas[1])
 
             # update relative coordinate info
             coords = frame.get_centers()
@@ -51,8 +56,47 @@ if __name__ == '__main__':
 
         # frame.show()
         # frame.show_contour()
-    # take area to be average of larges 20 frames
 
-    a1 = a1.sort()
-    a2 = a2.sort()
-    print(relCoords)
+    # take area to be average of larges 20 frames
+    areas1.sort()
+    areas1 = areas1[-20:]
+    areas2.sort()
+    areas2 = areas2[-20:]
+    a1 = sum(areas1) / len(areas1)
+    a2 = sum(areas2) / len(areas2)
+
+    # get relative movement from relative coordinates
+    relMovement = []
+    relEuclideanMovement = []
+    for i in range(1, len(relCoords)):
+        prev = relCoords[i - 1]
+        cur = relCoords[i]
+        relMovement.append((cur[0] - prev[0], cur[1] - prev[1]))
+        relEuclideanMovement.append(math.sqrt((cur[0] - prev[0]) ** 2 + (cur[1] - prev[1]) ** 2))
+
+    # print(a1)
+    # print(a2)
+    # print(relMovement)
+    # print(relEuclideanMovement)
+
+    return [a1, a2, relEuclideanMovement]
+
+
+if __name__ == '__main__':
+    X = np.empty((48, 3), dtype=object)
+    y = np.zeros(48)
+    for i in range(24):
+        print(i)
+        print(load_data("trajectories/t4"))
+        X[i] = load_data("trajectories/t4")
+        y[i] = 1
+    for i in range(24, 49):
+        print(i)
+        np.append(X, load_data("trajectories/t4"))
+        np.append(y, 1)
+
+    np.save("trainData_X", X, allow_pickle=True)
+    np.save("trainData_y", y, allow_pickle=True)
+
+    # model = Model(X, y)
+    # model.sequential()
